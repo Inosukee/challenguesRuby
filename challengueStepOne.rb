7dc71f8be63eebbcf2d583560a9c2f0178d091a0
Diego Alejandro Diaz Tocora <diego-diaz3@upc.edu.co>
2021-02-01 11:16:40 -0500

    Restructuracion codigo

--- challengueStepOne.rb
+++ challengueStepOne.rb
@@ -11,7 +11,7 @@ class ReadFiles
     lines.each do |line|
       temp = line.chars
       temp.each do |c|
-        if contexcepcion == 3 || contexcepcion == 7 || contexcepcion == 11 || contexcepcion == 15 || contexcepcion == 19 || contexcepcion == 23 || contexcepcion == 27 || contexcepcion == 31 || contexcepcion == 35
+        if [3, 7, 11, 15, 19, 23, 27, 31, 35].include? contexcepcion
           contexcepcion += 1
         else
           array[contline][contcaracter] = c
@@ -39,52 +39,28 @@ class ReadFiles
       init = 0
       for x in (1..9)
         #Found number 1 or 4
-        if array[contFileInit][init + 1] == " "
-          if array[contFileInit + 1][init] == " " && array[contFileInit + 1][init + 2] == "|" && array[contFileInit + 2][init + 2] == "|"
-            finalNumber = finalNumber + "1"
-          else
-            if array[contFileInit + 1][init] == "|" && array[contFileInit + 1][init + 1] == "_" && array[contFileInit + 1][init + 2] == "|" && array[contFileInit + 2][init + 2] == "|"
-              finalNumber = finalNumber + "4"
-            else
-              finalNumber = finalNumber + "?"
-            end
-          end
+        if array[contFileInit][init + 1].include? " "
+          finalNumber = [array[contFileInit + 1][init], array[contFileInit + 1][init + 2], array[contFileInit + 2][init + 2]] == ([" ", "|", "|"]) ? finalNumber + "1" :
+            finalNumber = [array[contFileInit + 1][init], array[contFileInit + 1][init + 1], array[contFileInit + 1][init + 2], array[contFileInit + 2][init + 2]] == (["|", "_", "|", "|"]) ? finalNumber + "4" : finalNumber + "?"
         else
+          case
           #Found number 9 or 8
-          if array[contFileInit + 1][init] == "|" && array[contFileInit + 1][init + 1] == "_" && array[contFileInit + 1][init + 2] == "|" && array[contFileInit + 2][init + 1] == "_" && array[contFileInit + 2][init + 2] == "|"
-            if array[contFileInit + 2][init] == " "
-              finalNumber = finalNumber + "9"
-            else
-              finalNumber = finalNumber + "8"
-            end
+          when [array[contFileInit + 1][init], array[contFileInit + 1][init + 1], array[contFileInit + 1][init + 2], array[contFileInit + 2][init + 1], array[contFileInit + 2][init + 2]] == ["|", "_", "|", "_", "|"]
+            finalNumber = array[contFileInit + 2][init] == " " ? finalNumber + "9" : finalNumber + "8"
             #Found number 0
-          elsif array[contFileInit + 1][init] == "|" && array[contFileInit + 1][init + 1] == " " && array[contFileInit + 1][init + 2] == "|" && array[contFileInit + 2][init + 1] == "_" && array[contFileInit + 2][init + 2] == "|" && array[contFileInit + 2][init] == "|"
+          when [array[contFileInit + 1][init], array[contFileInit + 1][init + 1], array[contFileInit + 1][init + 2], array[contFileInit + 2][init + 1], array[contFileInit + 2][init + 2], array[contFileInit + 2][init]] == ["|", " ", "|", "_", "|", "|"]
             finalNumber = finalNumber + "0"
             #Found number 5 or 6
-          elsif array[contFileInit + 1][init] == "|" && array[contFileInit + 1][init + 1] == "_" && array[contFileInit + 2][init + 1] == "_" && array[contFileInit + 2][init + 2] == "|"
-            if array[contFileInit + 2][init] == " "
-              finalNumber = finalNumber + "5"
-            else
-              finalNumber = finalNumber + "6"
-            end
+          when [array[contFileInit + 1][init], array[contFileInit + 1][init + 1], array[contFileInit + 2][init + 1], array[contFileInit + 2][init + 2]] == ["|", "_", "_", "|"]
+            finalNumber = array[contFileInit + 2][init] == " " ? finalNumber + "5" : finalNumber + "6"
             #Found number 2 or 3
-          elsif array[contFileInit + 1][init + 1] == "_" && array[contFileInit + 1][init + 2] == "|" && array[contFileInit + 2][init + 1] == "_"
-            if array[contFileInit + 2][init] == "|"
-              finalNumber = finalNumber + "2"
-            else
-              if array[contFileInit + 2][init + 1] == "_" && array[contFileInit + 2][init + 2] == "|"
-                finalNumber = finalNumber + "3"
-              else
-                finalNumber = finalNumber + "?"
-              end
-            end
-          else
+          when [array[contFileInit + 1][init + 1], array[contFileInit + 1][init + 2], array[contFileInit + 2][init + 1]] == ["_", "|", "_"]
+            finalNumber = array[contFileInit + 2][init] == "|" ? finalNumber + "2" : [array[contFileInit + 2][init + 1], array[contFileInit + 2][init + 2]] == ["_", "|"] ? finalNumber + "3" : finalNumber + "?"
             #Found number 7
-            if array[contFileInit][init + 1] == "_" && array[contFileInit + 1][init + 2] == "|" && array[contFileInit + 2][init + 2] == "|" && array[contFileInit + 2][init + 1] == " "
-              finalNumber = finalNumber + "7"
-            else
-              finalNumber = finalNumber + "?"
-            end
+          when [array[contFileInit][init + 1], array[contFileInit + 1][init + 2], array[contFileInit + 2][init + 2], array[contFileInit + 2][init + 1]] == ["_", "|", "|", " "]
+            finalNumber = finalNumber + "7"
+          else
+            finalNumber = finalNumber + "?"
           end
         end
         if x == 9
